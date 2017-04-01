27cda2d0c724ffd3133312e4f8ce7e190910c21a
Simon Emms <simon@simonemms.com>
2017-04-01 22:40:19 +0100

    Retrieved the AMI ID from the Packer build

---
+++ scripts/get_ami_id.py
@@ -0,0 +1,20 @@
+import json
+import re
+import sys
+
+file_name = sys.argv[1]
+
+ami_data = open(file_name, 'r').read().split('\n')
+
+amis = {}
+
+for line in ami_data:
+    ami = re.search('(ami-\w+)', line)
+
+    if ami:
+        lines = line.split(',')
+
+        if lines[2] == 'error' or lines[2] == 'artifact':
+            amis[lines[1]] = ami.group(0)
+
+print json.dumps(amis, indent=2)
