dfa3625801dfbeccd9aa043569a6ea6aef44ae08
Simon Emms <simon@simonemms.com>
2017-04-01 22:52:41 +0100

    Script to run the Terraform commands

---
+++ scripts/terraform.py
@@ -0,0 +1,42 @@
+"""
+Terraform
+
+Works out how many manager/worker instances
+to create and then runs the Terraform script
+"""
+import os
+import sys
+from subprocess import call
+
+cwd = sys.argv[1]
+
+config = {
+    'manager_instances': int(os.environ['TERRAFORM_MANAGER_INSTANCES'] or '0'),
+    'worker_instances': int(os.environ['TERRAFORM_WORKER_INSTANCES'] or '0'),
+}
+
+# Are we applying or destroying changes?
+if config['manager_instances'] == 0 and config['worker_instances'] == 0:
+    # Destroy
+    call([
+        'terraform',
+        'destroy',
+        '-force'
+    ], cwd=cwd)
+else:
+    # Apply
+    def call_terraform(action):
+        args = [
+            'terraform',
+            action
+        ]
+
+        for key in config:
+            value = config[key]
+
+            args.append('-var=' + key + '=' + str(value))
+
+        return call(args, cwd=cwd)
+
+    call_terraform('plan')
+    call_terraform('apply')
