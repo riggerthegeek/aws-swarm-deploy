7fdaa23d96a04dbafdbeff6e6f4d9522da16ec04
Simon Emms <simon@simonemms.com>
2017-04-09 22:08:23 +0100

    Getting a bit confused so saving the whole lot

---
+++ scripts/run.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env sh
+
+###########################################################
+###########################################################
+###                                                     ###
+###   Run                                               ###
+###                                                     ###
+###   This is a series of scripts that manage how       ###
+###   to set up a Swarm cluster. It builds a base       ###
+###   image using Packer (if nothing present or         ###
+###   current one too old), then provisions the AWS     ###
+###   boxes using Terraform. Finally, it configures     ###
+###   the Swarm cluster and deploys the Docker images   ###
+###   using Ansible                                     ###
+###                                                     ###
+###########################################################
+###########################################################
+
+errExit () {
+
+  if [ $1 \> 0 ]; then
+    echo "Script exited with $1"
+    exit $1
+  fi
+}
+
+if [ -z $AWS_KEY_PAIR ]; then
+  echo "Please provide a KEY_PAIR"
+  exit 1
+fi
+
+# Configure AWS
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY"
+aws configure set aws_secret_access_key "$AWS_SECRET_KEY"
+aws configure set output "json"
+aws configure set region "$AWS_REGION"
+
+##################
+##################
+###            ###
+###   Packer   ###
+###            ###
+##################
+##################
+
+echo "Creating a new AMI with Packer"
+
+rm -Rf ./packer/tmp
+mkdir -p ./packer/tmp
+cp ./packer/base/base.sh ./packer/tmp
+
+python ./scripts/generate_packer_file.py
+errExit $?
+
+# Validate the Packer stuff
+packer validate ./packer/tmp/base.json
+
+# Now run the Packer stuff
+packer build ./packer/tmp/base.json -machine-readable | tee ./packer-build.out
+
+AMI_LIST=$(python ./scripts/get_ami_id.py ./packer-build.out)
+
+rm -Rf ./packer-build.out ./packer/tmp
+
+echo "Packer step done"
+
+#####################
+#####################
+###               ###
+###   Terraform   ###
+###               ###
+#####################
+#####################
+
+echo "Provisioning the AWS instances using Terraform"
+
+# Use main AWS vars if not set
+if [ -z $TERRAFORM_ACCESS_KEY ]; then
+  TERRAFORM_ACCESS_KEY="$AWS_ACCESS_KEY"
+fi
+
+if [ -z $TERRAFORM_SECRET_KEY ]; then
+  TERRAFORM_SECRET_KEY="$AWS_SECRET_KEY"
+fi
+
+if [ -z $TERRAFORM_REMOTE_S3_BUCKET_REGION ]; then
+  TERRAFORM_REMOTE_S3_BUCKET_REGION="$AWS_REGION"
+fi
+
+# This is where we'll come back to
+
+# Configure the AWS variables
+TF_DIR="$PWD/terraform/swarm"
+TFVARS_FILE="$TF_DIR/terraform.tfvars"
+
+mkdir -p "$TF_DIR"
+
+rm -Rf "$TFVARS_FILE" "$TF_DIR/.terraform"
+
+echo "bucket = \"$TERRAFORM_REMOTE_S3_BUCKET_NAME\"" >> "$TFVARS_FILE"
+echo "key = \"$TERRAFORM_REMOTE_S3_BUCKET_KEY\"" >> "$TFVARS_FILE"
+echo "region = \"$TERRAFORM_REMOTE_S3_BUCKET_REGION\"" >> "$TFVARS_FILE"
+echo "access_key = \"$TERRAFORM_ACCESS_KEY\"" >> "$TFVARS_FILE"
+echo "secret_key = \"$TERRAFORM_SECRET_KEY\"" >> "$TFVARS_FILE"
+
+# Run the Terraform stuff
+python ./scripts/terraform.py "$TF_DIR" "$AMI_LIST" "$TFVARS_FILE"
+errExit $?
+
+echo "Terraform step done"
+
+###################
+###################
+###             ###
+###   Ansible   ###
+###             ###
+###################
+###################
+
+echo "Setting up Docker Swarm with Ansible"
+
+echo "Ansible step done"
--- scripts/terraform.py
+++ scripts/terraform.py
@@ -4,39 +4,103 @@ Terraform
 Works out how many manager/worker instances
 to create and then runs the Terraform script
 """
+import json
 import os
 import sys
+import time
+from string import Template
 from subprocess import call
 
 cwd = sys.argv[1]
+ami_list = json.loads(sys.argv[2])
+tfvars_file = sys.argv[3]
+
+access_key = os.environ['AWS_ACCESS_KEY']
+secret_key = os.environ['AWS_SECRET_KEY']
 
 config = {
     'manager_instances': int(os.environ['TERRAFORM_MANAGER_INSTANCES'] or '0'),
     'worker_instances': int(os.environ['TERRAFORM_WORKER_INSTANCES'] or '0'),
 }
 
+# Generate the files
+for region in ami_list:
+    print 'Generating region config: ' + region
+
+    # Get the base file
+    template_file = open(os.path.dirname(os.path.realpath(__file__)) + '/templates/terraform.txt')
+
+    src = Template(template_file.read())
+    output = src.substitute({
+        'access_key': access_key,
+        'ami': ami_list[region],
+        'availability_zone': 'b',
+        'base_instance': 't2.micro',
+        'key_pair': os.environ['AWS_KEY_PAIR'],
+        'manager_instances': config['manager_instances'],
+        'manager_instance_types': '{}',
+        'manager_name': '',
+        'module_name': 'swarm-' + region,
+        'region': region,
+        'secret_key': secret_key,
+        'worker_instances': config['worker_instances'],
+        'worker_instance_types': '{}',
+        'worker_name': ''
+    })
+
+    output_file = open(cwd + '/' + region + '.tf', 'w')
+    output_file.write(output)
+
+# Initialise the Terraform goodness
+code = call([
+    'terraform',
+    'init',
+    '-input=false',
+    '-backend=true',
+    '-backend-config=' + tfvars_file
+], cwd=cwd)
+
+call([
+    'terraform',
+    'get'
+], cwd=cwd)
+
+exit(1)
+
+if code > 0:
+    exit(code)
+
 # Are we applying or destroying changes?
 if config['manager_instances'] == 0 and config['worker_instances'] == 0:
     # Destroy
-    call([
+    print "Destroying Terraform infrastructure"
+
+    code = call([
         'terraform',
         'destroy',
         '-force'
     ], cwd=cwd)
+
+    if code > 0:
+        exit(code)
 else:
     # Apply
+    print "Applying Terraform infrastructure"
+
     def call_terraform(action):
         args = [
             'terraform',
             action
         ]
 
-        for key in config:
-            value = config[key]
+        exit_code = call(args, cwd=cwd)
 
-            args.append('-var=' + key + '=' + str(value))
+        exit(1)
 
-        return call(args, cwd=cwd)
+        if exit_code > 0:
+            exit(exit_code)
+
+        return exit_code
 
     call_terraform('plan')
     call_terraform('apply')
